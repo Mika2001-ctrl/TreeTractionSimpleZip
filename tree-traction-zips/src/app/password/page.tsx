2c389dbdefcc61d403b7d82367f9988f10397d87
Mika2001-ctrl <mika.naidu1@gmail.com>
2025-02-28 17:25:02 -0700

    Revert

--- tree-traction-zips/src/app/password/page.tsx
+++ tree-traction-zips/src/app/password/page.tsx
@@ -8,12 +8,10 @@ export default function PasswordPage() {
   const [error, setError] = useState("");
   const router = useRouter();
 
-  const appPassword = process.env.APP_PASSWORD;
-
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
-    const storedPassword = appPassword; // Retrieve from .env
+    const storedPassword = process.env.NEXT_PUBLIC_APP_PASSWORD; // Retrieve from .env
     if (password === storedPassword) {
       localStorage.setItem("isAuthenticated", "true"); // Store authentication
       router.push("/home"); // Redirect to home page
